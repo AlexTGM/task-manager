38310e5cbe5b2155856444018beb041b9569b686
a.mineev <alexandrmineev@gmail.com>
2020-05-14 01:02:11 +0300

    import graphql use vuex to load projects display projects on home view

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
+    ["env", {
+      "modules": false
+    }],
+    "stage-2"
   ],
+  "plugins": [
+    "graphql-tag",
+    "transform-vue-jsx",
+    "transform-runtime"
+  ]
 };
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,43 @@
 <template>
-  <div class="container">
-    <h1 class="title">
-      Hello World
-    </h1>
-    <p class="subtitle">
-      It's working
-    </p>
-  </div>
+<div class="container">
+  <table class="table is-fullwidth">
+    <thead>
+      <tr>
+        <th>Project</th>
+        <th>Description</th>
+        <th>Tasks</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="project in projects" :key="project.id">
+        <td>{{project.title}}</td>
+        <td>{{project.description}}</td>
+        <td>
+          <ul v-for="task in project.tasks" :key="task.id">
+            <li>{{task.title}}</li>
+          </ul>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+import { mapGetters } from 'vuex';
+
+import { Project } from '../store/models/Project';
+
+@Component({
+  computed: { ...mapGetters(['projects']) },
+
+  beforeMount() {
+    this.$store.dispatch('loadProjects');
+  },
+})
+export default class HomeComponent extends Vue {
+}
 </script>
